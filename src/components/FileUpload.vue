8417a53e4dfc565f430b4f6ed5bef042bc05151f
Jansi Rani <jansiphilip17@gmail.com>
2025-11-19 22:07:16 +0530

    Initial commit frontend

---
+++ src/components/FileUpload.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2>Upload PDF or CSV</h2>
+
+    <input type="file" @change="uploadFile" />
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "FileUpload",
+
+  methods: {
+  uploadFile(event) {
+  const formData = new FormData();
+  formData.append("file", event.target.files[0]);
+
+  axios.post("http://localhost:8000/upload/pdf/", formData, {
+    headers: { "Content-Type": "multipart/form-data" }
+  })
+  .then(res => console.log("Uploaded", res))
+  .catch(err => console.log("Upload Error:", err));
+}
+  },
+};
+</script>
